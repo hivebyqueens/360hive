efdf33a91bbdd3b67b531483cfd78b6fb4039e49
Muhinde234 <igirimpuhwedosta@gmail.com>
2026-04-02 22:12:22 +0200

    restructuring the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -271,7 +271,7 @@ export default function ContactPage() {
           </motion.div>
         </div>
 
-        {/* BOTTOM CTA */}
+        
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
