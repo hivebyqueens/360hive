08fba356fcbbfbf999093570cff16177b466acd5
Muhinde234 <igirimpuhwedosta@gmail.com>
2026-04-03 20:38:30 +0200

    structure the footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -86,7 +86,7 @@ export function Footer() {
           </div>
         </section>
 
-        {/* ── FOOTER GRID ── */}
+      
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 mb-24">
           
           {/* Brand Column */}
